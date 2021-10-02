941fd25314ee8ae1d3d0c4676896f62891925981
Jemerson19 <Jemersonalec19@gmail.com>
2021-10-02 16:26:37 -0300

    Alterações no Acompanhamento.vue da pasta views e nas informacoes.vue

--- src/views/Informacoes.vue
+++ src/views/Informacoes.vue
@@ -20,8 +20,34 @@ O mês de setembro foi escolhido porque é no dia 10 de setembro que se comemora
 
 Atualmente grande parte das capitais adere à campanha. Nesses casos, muitos monumentos e pontos turísticos da cidade ficam iluminados da cor amarela em apoio.</p>
 
+<img src="../assets/unnamed.jpg" alt="">
+<figcaption>Cristo Redentor iluminado com luz amarela, em apoio ao Setembro Amarelo.</figcaption>
+
+<h4 class="mt-4 mb-4">Objetivos do Setembro Amarelo
+</h4>
+<p>O principal objetivo do setembro amarelo é trazer visibilidade para o tema, e conscientizar a população para a prevenção de suicídios.
+
+Os veículos de mídia, a internet e as redes sociais tem conseguido trazer cada vez mais visibilidade para esse tema, que ainda é tratado como tabu em muitas regiões do país.
+
+A campanha visa trazer informação para a população, disponibilzando diversos materiais gratuitos pela internet, entrevistas para canais de comunicação. Esses materiais tratam desde dados sobre o suicídio até as formas de como abordar esse tema e identificar pessoas que precisam de ajuda.</p>
+
+<a href="https://www.setembroamarelo.com" target="_blank" rel="noopener">Se você quiser acessar o site oficial do Setembro Amarelo, clique aqui!</a>
+    
+    <h4 class="mt-4 mb-4"> Dados sobre o suicídio no Brasil e no Mundo
+</h4>
+<ul>
+  <li>São registrados 12 mil suicídios por ano no Brasil, sendo o oitavo país do mundo em números absolutos.</li>
+  <li class="mt-4 mb-4">Segundo a OMS, ocorrem cerca de 800 mil suicídios por ano no mundo.</li>
+  <p class="mt-4 mb-4">Sabe-se que o suicídio é a segunda maior causa de morte para jovens de 15-29 anos.</p>
+  <li>Estima-se que 96% das pessoas que cometem o ato contra sua própria vida sofriam com algum transtorno.</li>
+  <p class="mt-4 mb-4">Os suicídios estão fortemente ligados com a presença de transtornos mentais, e esse é um número que nos mostra a importância do tratamento psicológico para a população. Visto que muitas dessas doenças podem ser tratadas com um acompanhamento médico, o número de suicídios que poderiam ser prevenidos é incontável. Os transtornos mais comuns são depressão, bipolaridade e o abuso de substâncias.</p>
+
+</ul>
+    <p><a href="#" class="mt-4">Volta ao topo</a></p>
+    <p><a href="http://localhost:8084" class="mt-4">Volta ao Home</a></p>
 
     </v-container>
+
 
 </template>
 
